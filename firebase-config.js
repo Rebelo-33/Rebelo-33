293173167727a6217316cf92b48188c9e6f7b5fb
Rebelo-33 <85686836+Rebelo-33@users.noreply.github.com>
2025-07-20 22:17:43 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,10 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+// Import necessary Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA_vBdjuDi059d4-Yz-fg57fdZy_1GbmcA",
   authDomain: "gift-exchange-ab8bd.firebaseapp.com",
@@ -12,5 +15,8 @@ const firebaseConfig = {
   measurementId: "G-J98V9GEKFX"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Export Firestore instance to be used in other scripts
 export const db = getFirestore(app);
