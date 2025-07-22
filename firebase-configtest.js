7ac75b992dbed8f76ca564589bd3645fc5b882fc
Rebelo-33 <85686836+Rebelo-33@users.noreply.github.com>
2025-07-22 08:47:23 +0100

    Add files via upload

---
+++ firebase-configtest.js
@@ -0,0 +1,16 @@
+// ✅ firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_vBdjuDi059d4-Yz-fg57fdZy_1GbmcA",
+  authDomain: "gift-exchange-ab8bd.firebaseapp.com",
+  projectId: "gift-exchange-ab8bd",
+  storageBucket: "gift-exchange-ab8bd.appspot.com",
+  messagingSenderId: "371607822481",
+  appId: "1:371607822481:web:35fe7d620c5e5601271f30",
+  measurementId: "G-J98V9GEKFX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
